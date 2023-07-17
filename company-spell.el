12ea680385005dc59ee77a3072f595906e5fb4ed
enzu.ru <enzuru@users.noreply.github.com>
2023-07-17 12:49:23 -0400

    Fix broken function

--- company-spell.el
+++ company-spell.el
@@ -50,7 +50,7 @@
           trimmed-results '()))))
 
 ;;;###autoload
-(defun company-spell (command &optional arg)
+(defun company-spell (command &optional arg &rest ignored)
   "Run COMMAND and possibly ARG to accomplish spellchecking."
   (interactive (list 'interactive))
   (cl-case command
